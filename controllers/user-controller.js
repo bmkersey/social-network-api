10358e695bde903a9e48289ab2413daec165f5dc
Brendan Kersey <bmkersey@gmail.com>
2022-08-06 01:28:33 -0400

    set up bare bones user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,37 @@
+const { User } = require('../models');
+
+const userController = {
+  //get all users
+
+
+
+
+
+
+
+
+  //get one user by ID
+
+
+
+
+
+  //post new user
+
+
+
+
+  //put update user by ID
+
+
+  //delete remove user by ID BONUS: remove users thoughts when deleted.
+
+
+  //add friend
+
+
+  //remove friend
+
+
+
+}
